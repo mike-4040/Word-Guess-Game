c0f9d7ed87ea47d18ee50c24f6e19eaaaae0c6a3
Mike Kravtsov <mike4040@me.com>
2019-08-28 16:55:13 -0700

    Rewritten to comply with assignemnt

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # Word-Guess-Game
 UCSD Coda Camp JavaScript Assignment
+
+Rules of the game:
+
+1. The app randomly picks a letter, and the user has to guess which letter the app chose.
+
+2. Meaning of fiels on the screen:
+
+4. Wins: (# of times the user has guessed the letter correctly)
+
+5. Losses: (# of times the user has failed to guess the letter correctly after exhausting all guesses)
+
+6. Guesses Left: (# of guesses left. This will update)
+
+7. Your Guesses So Far: the specific letters that the user typed.
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,46 +5,45 @@ let lastGuess = '';
 let guesses = '';
 let secretLetter = randomLetter();
 
-const trysAllowed = 10;
+const trysAllowed = 5;
 
-let winsText = document.getElementById("wins");
-let lossesText = document.getElementById("losses");
-let guessesLeftText = document.getElementById("guesses-left");
-let guessesText = document.getElementById("guesses");
+let winsText = document.getElementById('wins');
+let lossesText = document.getElementById('losses');
+let guessesLeftText = document.getElementById('guesses-left');
+let guessesText = document.getElementById('guesses');
 
 guessesLeftText.textContent = trysAllowed;
 
-// guessesLeftText.textContent = 'Secret letter: ' + secretLetter + ', ';
-
-document.onkeyup = function(event) {
+document.onkeyup = function (event) {
   lastGuess = event.key;
   numberOfGueses++;
-  guesses = guesses + lastGuess;
+  guesses += lastGuess;
+
   if (lastGuess === secretLetter) {
-    wins++;  
-  } else {
+    wins++;
+    resetRound();
+  } else if (numberOfGueses >= trysAllowed) {
     losses++;
-  }
-  
-  if (numberOfGueses > trysAllowed) { //RESET
-    wins = 0;
-    numberOfGueses = 0;
-    losses = 0;
-    lastGuess = '';
-    secretLetter = randomLetter();
-    guesses = '';
+    resetRound();
   }
 
   winsText.textContent = wins;
   lossesText.textContent = losses;
   guessesLeftText.textContent = trysAllowed - numberOfGueses;
-  guessesText.textContent = 'SL: ' + secretLetter + ', ' + guesses;
-  
+  guessesText.textContent = guesses;
 };
 
 function randomLetter() {
   let aCode = 'a'.charCodeAt(0);
   let zCode = 'z'.charCodeAt(0);
 
-  return String.fromCharCode(aCode + Math.floor(Math.random()*(zCode-aCode)));
+  return String.fromCharCode(
+    aCode + Math.floor(Math.random() * (zCode - aCode))
+  );
+}
+
+function resetRound() {
+  numberOfGueses = 0;
+  secretLetter = randomLetter();
+  guesses = '';
 }
