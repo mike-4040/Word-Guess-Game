018b575f94aa783d444c3c4e889342152c38e1bc
Mike Kravtsov <mike4040@me.com>
2019-08-28 16:03:19 -0700

    Game complete, but for different rule :), will redo.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,21 +1,46 @@
+let wins = 0;
+let numberOfGueses = 0;
+let losses = 0;
+let lastGuess = '';
+let guesses = '';
+let secretLetter = randomLetter();
+
+const trysAllowed = 10;
+
 let winsText = document.getElementById("wins");
 let lossesText = document.getElementById("losses");
 let guessesLeftText = document.getElementById("guesses-left");
 let guessesText = document.getElementById("guesses");
-let lastGuess = "";
-let guesses = "";
-let secretLetter = "";
-let trysAllowed = 10;
-let numberOfGuess = 0;
 
+guessesLeftText.textContent = trysAllowed;
+
+// guessesLeftText.textContent = 'Secret letter: ' + secretLetter + ', ';
 
 document.onkeyup = function(event) {
   lastGuess = event.key;
-  winsText.textContent = event.key;
-  lossesText.textContent = randomLetter();
-};
+  numberOfGueses++;
+  guesses = guesses + lastGuess;
+  if (lastGuess === secretLetter) {
+    wins++;  
+  } else {
+    losses++;
+  }
+  
+  if (numberOfGueses > trysAllowed) { //RESET
+    wins = 0;
+    numberOfGueses = 0;
+    losses = 0;
+    lastGuess = '';
+    secretLetter = randomLetter();
+    guesses = '';
+  }
 
-randomLetter();
+  winsText.textContent = wins;
+  lossesText.textContent = losses;
+  guessesLeftText.textContent = trysAllowed - numberOfGueses;
+  guessesText.textContent = 'SL: ' + secretLetter + ', ' + guesses;
+  
+};
 
 function randomLetter() {
   let aCode = 'a'.charCodeAt(0);
