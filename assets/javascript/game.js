7842f94bf46a4765bb1b6bd72f81f85578cb0ebd
Mike Kravtsov <mike4040@me.com>
2019-08-28 10:08:20 -0700

    Random letter

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,25 @@
 let winsText = document.getElementById("wins");
+let lossesText = document.getElementById("losses");
+let guessesLeftText = document.getElementById("guesses-left");
+let guessesText = document.getElementById("guesses");
+let lastGuess = "";
+let guesses = "";
+let secretLetter = "";
+let trysAllowed = 10;
+let numberOfGuess = 0;
+
 
 document.onkeyup = function(event) {
+  lastGuess = event.key;
   winsText.textContent = event.key;
+  lossesText.textContent = randomLetter();
 };
+
+randomLetter();
+
+function randomLetter() {
+  let aCode = 'a'.charCodeAt(0);
+  let zCode = 'z'.charCodeAt(0);
+
+  return String.fromCharCode(aCode + Math.floor(Math.random()*(zCode-aCode)));
+}
